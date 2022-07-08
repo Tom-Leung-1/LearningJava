26756f676f483a507335a972540468889655bdb9
Tom-Leung-bb <wfleung8@cse.cuhk.edu.hk>
2022-07-08 20:39:56 +0800

    basics of private and public, static keyword, inner class

--- Review.java
+++ Review.java
@@ -1,34 +1,64 @@
-public class Review {
+public class Review { // your file name must match with the class name if public keyword is used. Otherwise, you can just remove the public keyword and choose another file name (see https://stackoverflow.com/questions/1841847/can-i-compile-a-java-file-with-a-different-name-than-the-class#:~:text=yes%2C%20you%20can%20choose%20any,which%20the%20main%20method%20exist.)
+    
     // access modifiers: public, private, protected, <No keyword>
     // public : everywhere
     // private : within class
     // protected : ?
     // <No keyword> : ?
 
+    public static int staticOne = 1;
+    public int one = 1;
 
-    // you cannot declare a public class inside the class with main method without the static keyword
-    public class Student {
-        private int cnt = 0;
-        public String name;
-        public Student() {
-            this.name = "Tom";
+    class InnerClassOne {
+        public int cnt = 0;
+        InnerClassOne() {
+            System.out.println(one);
         }
-        public Student(String name) {
-            this.name = name;
+    }
+
+    static class InnerClassTwo { // static nested class only has access to static members of the outer class. (the main difference between non-static inner class and static nested class)
+        public int cnt = 0;
+        InnerClassTwo() {
+            // System.out.println(one); // error 
+            System.out.println(staticOne); // OK
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        test();
-        Student s1 = new Student();
-        Student s2 = new Student("Kevin");
-        System.out.println(s1.cnt);
-        System.out.println(s2.name);
+        // you can create the object of a class inside the same class itself
+        Review review = new Review();
+        // to create the inner class object of another class, use the syntax below
+        InnerClassOne c1 = review.new InnerClassOne();
+        // you can also create the inner class object if you add static keword into the inner class. It is also called static nested class
+        InnerClassTwo c2 = new InnerClassTwo();
+        // you can also create an object of a class by: 
+        //  1.  adding another java file into the same directory
+        //  2.  create an instance in the main method 
+        Teacher t1 = new Teacher();
+        Teacher t2 = new Teacher("Kevin");
+
+        staticMethod();
+        review.method();
+
+        // cnt is the public static variable. It can be modified everywhere and every instance of Teacher class share the same static variable 
+        t1.cnt = 3;
+        System.out.println(t2.cnt);
+        
+        // we cannot modify the private variable in another class. 
+        // t1.privateCnt = 3; // error
+
+        
+
     }
 
-    public static void test() {
-        System.out.println("testing");
+    // static method = class method -> we don't need to specifically create an instance to call this method
+    public static void staticMethod() {
+        System.out.println("call method 1");
     }
+
+    public void method() {
+        System.out.println("call method 2");
+    }
+
 }
 
--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,21 @@
 public class Teacher {
-    
+    public String name;
+    // if you don't initialize your static variable, the default value of it is 0.
+    public static int cnt = 0;
+    private static int privateCnt = 0;
+
+    public Teacher() {
+        this.name = "Tom";
+    }
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public class TeacherPet {
+        public int sticks;
+        TeacherPet() {
+            this.sticks = 4;
+        }
+    }
 }
